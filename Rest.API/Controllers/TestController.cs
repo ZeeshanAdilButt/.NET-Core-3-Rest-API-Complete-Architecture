45c68097a6841f961c6880c5bb0e24398fefa855
zeeshanadilbutt <zeeradiations@outlook.com>
2020-03-30 01:02:54 +0500

    basic API setup with Swagger for API documentation

---
+++ Rest.API/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rest.API.Controllers
+{
+    public class TestController : Controller
+    {
+
+        [HttpGet("api/user")]
+        public IActionResult Get() {
+
+            return Ok(new { name = "zee" });
+
+        }
+    }
+}
